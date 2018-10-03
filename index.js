66b09904135e7441219dafcc85c4b25da32d26fa
Kris Linquist <kris@linquist.net>
2018-10-03 16:54:37 -0700

    Also cache pressure for 2min

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Nodecache = require('node-cache');
 const geohash = require('ngeohash');
 const cache = new Nodecache({ stdTTL: 604800 });
+const pressureCache = new Nodecache({
+    stdTTL: 120
+});
 const wu = new require('./lib/wu')({
     apiKey: 'xxxxxxx'
 });
@@ -38,11 +41,19 @@ server.get('/findairport/:lat/:long', (req, res) => {
 server.get('/getairportdata/:icao', (req, res) => {
     if (!req.params || !req.params.icao) return res.send(500);
     cl(`${req.connection.remoteAddress} Received request to get airport data for ${req.params.icao}`);
+    const cacheResult = pressureCache.get(req.params.icao);
+    if (cacheResult) {
+        cl(`Returning pressure result from cache: ${cacheResult}`);
+        res.send(200, {
+            data: cacheResult
+        });
+    }
     wu.getAirportData(req.params.icao, (err, result) => {
         if (err) {
             cl(`Error from wu: ${err}`);
             return res.send(500);
         }
+        pressureCache.set(req.params.icao, result);
         cl(`Returning airport data result: ${JSON.stringify(result)}`);
         res.send(200, {data: result});
     });
